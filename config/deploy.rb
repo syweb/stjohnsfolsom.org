12453f45f0640c57de2e264a444901cbaedfdf1b
Brian <brian@insightify.com>
2011-02-15 12:00:06 -0800

    [MISC]

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :repository,  "git@github.com:syweb/stjohnfolsom.org.git"
+set :repository,  "git@github.com:syweb/stjohnsfolsom.org.git"
 set :user,        "sites"
 set :domain,      "#{user}@insightmethods.com"
 set :deploy_to,   "/home/sites/stjohnfolsom.org/test"
@@ -7,7 +7,6 @@ set :deploy_to,   "/home/sites/stjohnfolsom.org/test"
 
 desc "this is for production"
 task :production do
-  set :repository,  "git@github.com:insightmethods/stjohnfolsom.org.git"
   set :deploy_to,   "/home/sites/stjohnfolsom.org"
 end
 
