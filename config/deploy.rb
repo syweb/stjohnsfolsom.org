b0550b89485b567a5c84a9140683170abb69dd31
Chintan Sheth <chintansheth@Chintan-Sheths-MacBook.local>
2011-03-02 18:56:07 +0530

    [FIX] change to new deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,12 @@
-set :repository,  "git@github.com:syweb/stjohnsfolsom.org.git"
+set :application, "stjohnfolsom.org"
+set :repository,  "git@github.com:syweb/#{application}.git"
 set :user,        "sites"
 set :domain,      "#{user}@insightmethods.com"
-set :deploy_to,   "/home/sites/stjohnfolsom.org/current/public/test"
-
+set :deploy_to,   "/home/sites/#{application}/current/public/test"
+set :revision,    "origin/master"
 
 
 desc "this is for production"
 task :production do
   set :deploy_to,   "/home/sites/stjohnfolsom.org"
-end
-
-
-namespace :vlad do
-
 end
