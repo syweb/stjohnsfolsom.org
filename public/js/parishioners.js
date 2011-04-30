e14abe7deecc1b995e4bb2edaa73d5c30021e9bd
Brian <brian@insightify.com>
2011-04-29 17:32:41 -0700

    [MISC] update sy integration
    Signed-off-by: Brian <brian@insightify.com>

--- public/js/parishioners.js
+++ public/js/parishioners.js
@@ -1,4 +1,6 @@
 var PARISH_URL = 'https://stjohnfolsom.schoolyardapp.com';
+//var PARISH_URL = 'https://saintjosephredding.schoolyardapp.com';
+//var PARISH_URL = 'http://stjohnfolsom.local-sy.com:4001';
 
 function for_accordian(accordian_no){
 
@@ -41,65 +43,12 @@ function for_accordian(accordian_no){
 	//Accordian End
 }
 
-function get_announcements(div_to_append) {
-  $.ajax({
-    url: PARISH_URL + "/get_announcements.json",
-    cache: false,
-    type: 'get',
-    dataType: 'jsonp',
-    success: function(response) {
-      $.each(response, function(i, item) {
-        var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
-        $(div_to_append).append('<div class="data-block long-block">');
-        $(div_to_append).append('<h5>' + response[i].title + '</h5>');
-        $(div_to_append).append('<p>' + 'Posted on :: ' + dt + '</p>');
-        $(div_to_append).append(response[i].content);
-        $(div_to_append).append('</div>');
 
-      });
-    }
-  });
-
-}
-
-
-
-function get_events(div_to_append) {
-  $.ajax({
-    url: PARISH_URL + "/get_events.json",
-    cache: false,
-    type: 'get',
-    dataType: 'jsonp',
-    success: function(response) {
-      $.each(response, function(i, item) {
-        var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
-        $(div_to_append).append('<div class="data-block long-block">');
-        $(div_to_append).append('<h5>' + response[i].title + '</h5>');
-        $(div_to_append).append('<p>' + 'Posted on :: ' + dt + '</p>');
-        $(div_to_append).append(response[i].description);
-        $(div_to_append).append('</div>');
-
-      });
-    }
-  });
-
-}
-
-
-// function get_group_bulletins(div_to_append, group_id, number) {
 function get_group_bulletins(div_to_append, group_id, sort_by_field, order_by_value, number) {
   
-  if (typeof sort_by_field  == "undefined"){
-    sort_by_field = "title";
-  }
-  
-  if (typeof order_by_value == "undefined"){
-    order_by_value = "asc";
-  }
-
-  if (typeof number == "undefined") {
-      number = 6;
-  }
+  if (typeof sort_by_field  == "undefined"){ sort_by_field = "title"; }  
+  if (typeof order_by_value == "undefined"){ order_by_value = "asc"; }
+  if (typeof number == "undefined") { number = 6; }
 
   $.ajax({
     url: PARISH_URL + "/get_group_announcements.json/" + group_id + "/"+ sort_by_field + "/" + order_by_value,
@@ -138,17 +87,9 @@ function get_group_bulletins(div_to_append, group_id, sort_by_field, order_by_va
 
 function get_group_announcements(div_to_append, group_id, sort_by_field, order_by_value, number) {
   
-  if (typeof sort_by_field  == "undefined"){
-    sort_by_field = "title";
-  }
-  
-  if (typeof order_by_value == "undefined"){
-    order_by_value = "asc";
-  }
-
-  if (typeof number == "undefined") {
-      number = 6;
-  }
+  if (typeof sort_by_field  == "undefined"){ sort_by_field = "title"; }
+  if (typeof order_by_value == "undefined"){ order_by_value = "asc"; }
+  if (typeof number == "undefined") { number = 6; }
 
   $.ajax({
     url: PARISH_URL + "/get_group_announcements.json/" + group_id + "/"+ sort_by_field + "/" + order_by_value,
@@ -156,55 +97,65 @@ function get_group_announcements(div_to_append, group_id, sort_by_field, order_b
     type: 'get',
     dataType: 'jsonp',
     success: function(response) {
-        if (response != ""){
-             $(div_to_append).append('<div id="accordion" class="event-box"><div class="box-title">Announcements</div></div>');
-        }
+      show_announcement(response, div_to_append, number);
+    }
+  });
 
-      $.each(response, function(i, item) {
-        var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
+}
 
-        var event_link = 'event-link-' + response[i].id;
+function get_home_page_announcements(div_to_append, sort_by_field, order_by_value, number) {
+  
+  if (typeof sort_by_field  == "undefined"){ sort_by_field = "title"; }
+  if (typeof order_by_value == "undefined"){ order_by_value = "asc"; }
+  if (typeof number == "undefined") { number = 6; }
 
-        if (i < number) {
-
-          $(div_to_append).find('.event-box').append('<div class="event-title "><a >' + response[i].title + '</a></div>');
-          $(div_to_append).find('.event-box').append('<div class="event-dtail ">' + response[i].content + '<span id="' + event_link + '"></span></div>');
-
-          if (response[i].has_downloads == true) {
-            download_link = PARISH_URL + '/publik/download/' + response[i].id;
-            $(div_to_append).find('#'+event_link).append('<br/><a href="' + download_link + '">Download Attachment</a>');
-          }
-        }
-
-      });
-         if (response != ""){
-              $(div_to_append).append('<div class="event-box-sptr"></div>');
-            }
-	  for_accordian('accordian_1');
+  $.ajax({
+    url: PARISH_URL + "/get_home_page_announcements.json/" + sort_by_field + "/" + order_by_value,
+    cache: false,
+    type: 'get',
+    dataType: 'jsonp',
+    success: function(response) {
+      show_announcement(response, div_to_append, number);
     }
   });
 
 }
 
 
+function show_announcement(response, div_to_append, number) {
+  if (response != ""){
+       $(div_to_append).append('<div id="accordion" class="event-box"><div class="box-title"><h3>Announcements</h3></div></div>');
+  }
+
+  $.each(response, function(i, item) {
+    var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
+
+    var event_link = 'event-link-' + response[i].id;
+
+    if (i < number) {
+
+      $(div_to_append).find('.event-box').append('<div class="event-title "><strong>' + response[i].title + '</strong></div>');
+      $(div_to_append).find('.event-box').append('<div class="event-dtail ">' + response[i].content + '<span id="' + event_link + '"></span></div>');
+
+      if (response[i].has_downloads == true) {
+        download_link = PARISH_URL + '/publik/download/' + response[i].id;
+        $(div_to_append).find('#'+event_link).append('<br/><a href="' + download_link + '">Download Attachment</a>');
+      }
+    }
+
+  });
+  if (response != ""){
+    $(div_to_append).append('<div class="event-box-sptr"></div>');
+  }
+  for_accordian('accordian_1');
+}
 
 function get_group_events(div_to_append, group_id, sort_by_field, order_by_value, number, group_name) {
   
-  if (typeof sort_by_field  == "undefined"){
-    sort_by_field = "title";
-  }
-  
-  if (typeof order_by_value == "undefined"){
-    order_by_value = "asc";
-  }
-
-  if (typeof number == "undefined") {
-      number = 6;
-  }
-  
-  if (typeof group_name == "undefined") {
-      group_name = "Upcoming Events";
-  }
+  if (typeof sort_by_field  == "undefined"){ sort_by_field = "title";}
+  if (typeof order_by_value == "undefined"){ order_by_value = "asc"; }
+  if (typeof number == "undefined") { number = 6; }
+  if (typeof group_name == "undefined") { group_name = "Upcoming Events"; }
  
 
   $.ajax({
@@ -212,52 +163,90 @@ function get_group_events(div_to_append, group_id, sort_by_field, order_by_value
     cache: false,
     type: 'get',
     dataType: 'jsonp',
-    success: function(response) {
-        if (response != ""){
-          $(div_to_append).append('<div id="accordion" class="cls-events"><h4>' + group_name + '</h4><ul id="events" ></ul>');
-        }
-
-      // $.each(response.reverse(), function(i, item) {
-      $.each(response, function(i, item) {
-        var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
-
-        date_string = get_event_date(response[i].start_date, response[i].start_time, response[i].end_date, response[i].end_time);
-        var event_link = 'event-link-' + response[i].id;
-
-
-
-        detail = '';
-        if (response[i].description == 'NA') {
-          detail = '<div class="event-dtail"><span id="' + event_link + '"></span></div>';
-        } else {
-          detail = '<div class="event-dtail"><br/>' + response[i].description + '<span id="' + event_link + '"></span></div>';
-        }
-
-        if (i < number) {
-          $(div_to_append).find('#events').append('<li><div class="event-title "><div class="date">' + date_string + '</div><div class="event-title-text"><a>' + response[i].title + '</a></div><div class="sptr1"></div></div>'
-          + detail
-          + '</li>');
-
-          if (response[i].has_downloads == true) {
-            download_link = PARISH_URL + '/publik/download/' + response[i].id;
-            $(div_to_append).find('#'+event_link).append('<br/><a href="' + download_link + '">Download Attachment</a>');
-          }
-
-
-        }
-
-      });
-
-      $(div_to_append).append('</div>');
-      if (response != ""){
-        $(div_to_append).append('<div class="event-box-sptr"></div>');
-        }
-	  for_accordian('accordian_2');
+    success: function(response) { 
+      show_event(response, div_to_append, number, group_name); 
     }
   });
 
 }
 
+
+
+function get_home_page_events(div_to_append, sort_by_field, order_by_value, number, group_name) {
+  
+  if (typeof sort_by_field  == "undefined"){ sort_by_field = "title";}
+  if (typeof order_by_value == "undefined"){ order_by_value = "asc"; }
+  if (typeof number == "undefined") { number = 6; }
+  if (typeof group_name == "undefined") { group_name = "Upcoming Events"; }
+ 
+
+  $.ajax({
+    url: PARISH_URL + "/get_home_page_events.json/" + sort_by_field + "/" + order_by_value,
+    cache: false,
+    type: 'get',
+    dataType: 'jsonp',
+    success: function(response) { 
+      show_event(response, div_to_append, number, group_name); 
+    }
+  });
+
+}
+
+function show_event(response, div_to_append, number, group_name) {
+  if (response != ""){
+    $(div_to_append).append('<div id="accordion" class="cls-events"><h3>' + group_name + '</h3><ul id="events" ></ul>');
+  }
+
+  $.each(response, function(i, item) {
+    var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
+
+    date_string = get_event_date(response[i].start_date, response[i].start_time, response[i].end_date, response[i].end_time);
+    var event_link = 'event-link-' + response[i].id;
+    detail = '';
+    if (response[i].description == 'NA') {
+      detail = '<div class="event-dtail"><span id="' + event_link + '"></span></div>';
+    } else {
+      detail = '<div class="event-dtail"><br/>' + response[i].description + '<span id="' + event_link + '"></span></div>';
+    }
+
+    if (i < number) {
+      $(div_to_append).find('#events').append('<li><div class="event-title "><div class="date"><em>' + date_string + '</em></div><div class="event-title-text">' + response[i].title + '</div><div class="sptr1"></div></div>'
+      + detail
+      + '</li>');
+
+      if (response[i].has_downloads == true) {
+        download_link = PARISH_URL + '/publik/download/' + response[i].id;
+        $(div_to_append).find('#'+event_link).append('<br/><a href="' + download_link + '">Download Attachment</a>');
+      }
+    }
+
+  });
+
+  $(div_to_append).append('</div>');
+  if (response != ""){
+    $(div_to_append).append('<div class="event-box-sptr"></div>');
+  }
+  for_accordian('accordian_2');
+}
+
+
+
+
+function get_home_page_forms(div_to_append, group_id, group_name, number) {
+
+  if (typeof group_name == "undefined") { group_name = "Files and Form"; }
+  
+  $.ajax({
+    url: PARISH_URL + "/get_home_page_forms.json/",
+    cache: false,
+    type: 'get',
+    dataType: 'jsonp',
+    success: function(response) {
+      show_form(response, div_to_append, group_id, 'Files and Forms', number);
+    }
+  });
+
+}
 
 function get_group_forms(div_to_append, group_id, group_name, number) {
 
@@ -267,28 +256,31 @@ function get_group_forms(div_to_append, group_id, group_name, number) {
     type: 'get',
     dataType: 'jsonp',
     success: function(response) {
-
-
-
-      $(div_to_append).append('<div id="accordion" class="cls-events"><div class="box-title">&nbsp;&nbsp;&nbsp;<em>' + group_name + '</em></div><ul id="events" ></ul>');
-
-      $.each(response.reverse(), function(i, item) {
-        var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
-
-        date_string = get_event_date(response[i].start_date, response[i].start_time, response[i].end_date, response[i].end_time);
-        download_link = PARISH_URL + '/publik_file_download/' + response[i].id;
-
-
-        $(div_to_append).find('#events').append('<li><div><div class="event-title"><a href="' + download_link + '">' + response[i].title + '</a></div><div class="sptr1"></div></div>'
-        + '</li>');
-      });
-
-      $(div_to_append).append('</div>');
-	    //for_accordian('accordian_2');
+      show_form(response, div_to_append, group_id, group_name, number);
     }
   });
 
 }
+
+function show_form(response, div_to_append, group_id, group_name, number) {
+    
+  $(div_to_append).append('<div id="accordion" class="cls-events"><div class="box-title">&nbsp;&nbsp;&nbsp;<em>' + group_name + '</em></div><ul id="events" ></ul>');
+
+  $.each(response.reverse(), function(i, item) {
+    var dt = response[i].updated_at.replace(/T|Z/g, " ").replace(/-/g, "/");
+
+    date_string = get_event_date(response[i].start_date, response[i].start_time, response[i].end_date, response[i].end_time);
+    download_link = PARISH_URL + '/publik_file_download/' + response[i].id;
+
+
+    $(div_to_append).find('#events').append('<li><div><div class="event-title"><a href="' + download_link + '">' + response[i].title + '</a></div><div class="sptr1"></div></div>'
+    + '</li>');
+  });
+
+  $(div_to_append).append('</div>');
+}
+
+
 
 
 
