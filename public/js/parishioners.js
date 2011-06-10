9259c15ce1151ec93abfd550b0fce7d3acb8c4ee
Chintan Sheth <chintansheth@Chintan-Sheths-MacBook.local>
2011-06-10 12:00:30 +0530

    [Change] make change on header secession

--- public/js/parishioners.js
+++ public/js/parishioners.js
@@ -300,5 +300,5 @@ function get_event_date(start_date, start_time, end_date, end_time) {
 
 // This is for header event icon
 function get_event_icon(){
-	$('.header-full .container').append('<div id="event-icon" ><a href="index.html" title="Summer"><img src="images/event-icon-spring.png" alt="Summer" /> </a></div>')
+	$('.header-full .container').append('<div id="event-icon" ><a href="index.html" title="Summer"><img src="images/event-icon-summer.png" alt="Summer" /> </a></div>')
 }
