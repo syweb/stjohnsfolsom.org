5a682a8876cb5188b333601e9ba8573a2366e1d4
Dhiraj <dhiraj@insightmethods.com>
2015-05-19 16:11:28 +0530

    update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 set :application, "stjohnfolsom.org"
 set :repository,  "git@github.com:syweb/stjohnsfolsom.org.git"
-set :user,        "sites"
+set :user,        "ubuntu"
 set :domain,      "#{user}@insightmethods.com"
-set :deploy_to,   "/home/sites/#{application}/current/public/test"
+set :deploy_to,   "/home/#{user}/sites/#{application}/current/public/test"
 set :revision,    "origin/master"
 
 
 desc "this is for production"
 task :production do
-  set :deploy_to,   "/home/sites/#{application}"
+  set :deploy_to,   "/home/#{user}/sites/#{application}"
   set :keep_releases, 3
 end
